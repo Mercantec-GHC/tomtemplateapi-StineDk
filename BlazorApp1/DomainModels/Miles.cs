4e3932bc30cfd215cb67874901308003b0aae9d0
StineDk <144903306+StineDk@users.noreply.github.com>
2024-08-07 11:34:39 +0200

    Miles95 Chart works

---
+++ BlazorApp1/DomainModels/Miles.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorApp1.DomainModels
+{
+	public class Miles
+	{
+		public DateTime date { get; set; }
+		public decimal price { get; set; }
+	}
+}
--- BlazorApp1/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
+++ BlazorApp1/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e46d047bb9e076f6de6520c49f1ebdf66374b36")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
